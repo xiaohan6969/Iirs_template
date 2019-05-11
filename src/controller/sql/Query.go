89fe71ad68c893924f5d367dc1e7664441bde7e3
xiaohan6969 <948309461@qq.com>
2019-05-11 15:27:51 +0800

    d

--- src/controller/sql/Query.go
+++ src/controller/sql/Query.go
@@ -10,25 +10,25 @@ import (
 type Query struct{}
 
 func (a *Query) BeforeActivation(b mvc.BeforeActivation) {
+	b.Handle("GET", "/query", "Query")
 	b.Handle("GET", "/test", "Test")
 }
 
-func (a *Query) Test(ctx iris.Context) {
+func (a *Query) Query(ctx iris.Context) {
 	db := mysql.Mysql
 	//关闭数据库，db会被多个goroutine共享，可以不调用
 	defer db.Close()
 	//查询数据，指定字段名，返回sql.Rows结果集
-	rows2, _ := db.Query("select * from 宁波港口指数")
+	rows2, _ := db.Query("select * from test")
 	fmt.Println("===", rows2)
+	ctx.JSON(rows2)
 }
+
+func (a *Query) Test(ctx iris.Context) {
+
+}
+
 func (a *Query) Insert(ctx iris.Context) {
 	//create table test(id int primary key auto_increment, Name varchar(18),  sex varchar(2),  age int);
 	//insert into test(Name,sex,age) values("2","3",12);
-}
-
-func (a *Query) Find(ctx iris.Context) {
-	//db := mysql.Mysql
-}
-
-func (a *Query) Query(ctx iris.Context) {
 }
