249402b1ccec6cf167ae90d700acc3c5acc59706
xiaohan6969 <948309461@qq.com>
2019-05-11 18:41:20 +0800

    2019.5.11 over_commit

--- src/controller/sql/Query.go
+++ src/controller/sql/Query.go
@@ -16,18 +16,50 @@ var (
 )
 
 func (a *Query) BeforeActivation(b mvc.BeforeActivation) {
-	b.Handle("GET", "/query", "Query")
+	//b.Handle("GET", "/query", "Query")
 	b.Handle("GET", "/test", "Test")
 }
 
-func (a *Query) Query(ctx iris.Context) interface{} {
-	db := mysql.Mysql
-	//关闭数据库，db会被多个goroutine共享，可以不调用
-	//defer db.Close()
-	//查询数据，指定字段名，返回sql.Rows结果集
-	//sql := "select * from " + database
+//func (a *Query) Query(ctx iris.Context) []model.Test {
+//	//关闭数据库，db会被多个goroutine共享，可以不调用
+//	//defer db.Close()
+//	//查询数据，指定字段名，返回sql.Rows结果集
+//	//sql := "select * from " + database
+//	db := mysql.Mysql
+//	sql := "select id,name,sex,age from " + database
+//	querySet, err := db.Query(sql)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	//res := model.Test{}
+//	var result []model.Test
+//	for querySet.Next() {
+//		var name, sex string
+//		var id, age int
+//		err = querySet.Scan(
+//			&id,
+//			&name,
+//			&sex,
+//			&age)
+//		result = append(result, model.Test{
+//			id,
+//			name,
+//			sex,
+//			age,
+//		})
+//	}
+//	defer func() {
+//		if err := querySet.Close(); err != nil {
+//			fmt.Println("close fail")
+//		}
+//	}()
+//	return result
+//}
+
+func (a *Query) Test(ctx iris.Context) []model.Test {
+	db := mysql.GetDataBase()
 	sql := "select id,name,sex,age from " + database
-	querySet, err := db.Query(sql)
+	querySet, err := db.DB().Query(sql)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -39,14 +71,19 @@ func (a *Query) Query(ctx iris.Context) interface{} {
 			&res.Name,
 			&res.Sex,
 			&res.Age)
-		result = append(result, res)
+		result = append(result, model.Test{
+			res.Id,
+			res.Name,
+			res.Sex,
+			res.Age,
+		})
 	}
-	err = querySet.Close()
+	defer func() {
+		if err := querySet.Close(); err != nil {
+			fmt.Println("close fail")
+		}
+	}()
 	return result
-}
-
-func (a *Query) Test(ctx iris.Context) {
-
 }
 
 func (a *Query) Insert(ctx iris.Context) {
--- src/server/databases/mysql/init.go
+++ src/server/databases/mysql/init.go
@@ -2,25 +2,48 @@ package mysql
 
 import (
 	"config"
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+//var (
+//	Mysql *sql.DB
+//)
+//
+//func init() {
+//	user := config.Config.Get("mysql.user").(string)
+//	password := config.Config.Get("mysql.password").(string)
+//	database := config.Config.Get("mysql.database").(string)
+//	host := config.Config.Get("mysql.host").(string)
+//	port := config.Config.Get("mysql.port").(string)
+//	//db, err := sql.Open("mysql", "数据库账号:数据库密码@tcp(数据库Ip:端口)/数据库名称?parseTime=true")
+//	db, err := sql.Open("mysql", user+":"+password+"@tcp("+host+":"+port+")/"+database+"?charset=utf8&parseTime=true&loc=Local")
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	Mysql = db
+//}
+
 var (
-	Mysql *sql.DB
+	db  *gorm.DB
+	ERR error
 )
 
 func init() {
 	user := config.Config.Get("mysql.user").(string)
-	password := config.Config.Get("mysql.password").(string)
-	database := config.Config.Get("mysql.database").(string)
-	host := config.Config.Get("mysql.host").(string)
-	port := config.Config.Get("mysql.port").(string)
-	//db, err := sql.Open("mysql", "数据库账号:数据库密码@tcp(数据库Ip:端口)/数据库名称?parseTime=true")
-	db, err := sql.Open("mysql", user+":"+password+"@tcp("+host+":"+port+")/"+database+"?charset=utf8&parseTime=true&loc=Local")
-	if err != nil {
-		fmt.Println(err)
+	pwd := config.Config.Get("mysql.password").(string)
+	databases := config.Config.Get("mysql.database").(string)
+	addr := config.Config.Get("mysql.host").(string)
+	db, ERR = gorm.Open("mysql", user+":"+pwd+"@tcp("+addr+")/"+databases+"?charset=utf8&parseTime=true&loc=Local")
+	if ERR != nil {
+		panic("--- 数据库连接失败")
 	}
-	Mysql = db
+	db.DB().SetMaxIdleConns(500)
+	db.DB().SetMaxOpenConns(5000)
+	db.DB().SetConnMaxLifetime(0)
+	db.LogMode(true)
+}
+
+func GetDataBase() *gorm.DB {
+	return db
 }
