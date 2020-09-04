1ffc296803e9736f839d371e5fb7887ff9f08919
xiaohan6969 <948309461@qq.com>
2020-09-04 14:34:48 +0800

    1

--- controller/testTemplate/test.go
+++ controller/testTemplate/test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
 	"log"
+	"strconv"
 )
 
 type SqlNature struct{}
@@ -63,17 +64,51 @@ func (a *SqlNature) IndexList(ctx iris.Context) iris.Map {
 }
 
 
-func (a *SqlNature) OneDetail(ctx iris.Context) {
+func (a *SqlNature) OneDetail(ctx iris.Context) (iris.Map) {
+	db := config2.Mysql
 	type request struct {
-		IndexId string `json:"index_id"`
+		IndexId int `json:"index_id"`
 	}
 	values := request{}
 	err := ctx.ReadJSON(&values)
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(values)
-	fmt.Println(values)
+	//查询数据，指定字段名，返回sql.Rows结果集
+	sql := "select id,first_name,last_name from "+table1 +" where id = "+ strconv.Itoa(values.IndexId)
+	type DetailedQuery struct {
+		Id int `sql:"id"`
+		FirstName string `sql:"first_name"`
+		LastName string `sql:"last_name"`
+	}
+	querySet, err := db.Query(sql)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := DetailedQuery{}
+	var result []interface{}
+	for querySet.Next() {
+		err = querySet.Scan(
+			&res.Id,       //字段1
+			&res.FirstName,       //字段1
+			&res.LastName,       //字段2
+		)
+		result = append(result,DetailedQuery{
+			res.Id,
+			res.FirstName,
+			res.LastName,
+		})
+	}
+	defer func() {
+		if err := querySet.Close(); err != nil {
+			fmt.Println("close fail")
+		}
+	}()
+	return iris.Map{
+		"status":  200,
+		"data": res,
+		"message": "1111",
+	}
 }
 
 func (a *SqlNature) Test() {}
