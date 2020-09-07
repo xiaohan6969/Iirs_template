20429339962c00adda1b3c78856a4029f7a7adff
xiaohan6969 <948309461@qq.com>
2020-09-07 13:50:16 +0800

    1

--- model/userModel/userModel.go
+++ model/userModel/userModel.go
@@ -13,13 +13,6 @@ func RegisterNewUserModel(user_name,pass_word string) (string,error) {
 		token string
 	)
 	token,_ = jwt.CreateToken(&jwt.Claims{UserName: user_name,})
-	//aaa := jwt.Claims{
-	//UserId: 1,
-	//}
-	//ccc,bbb := jwt.CreateToken(&aaa)
-	//fmt.Println(ccc,bbb)
-	//ddd,bb := jwt.ValidateToken(ccc)
-	//fmt.Println(ddd.UserId,bb)
 	user := &commonStruct.User{
 		UserName:user_name,
 		PassWord:pass_word,
