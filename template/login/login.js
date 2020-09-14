456fb4afe76e3779a67821c97f5462b7209341d7
xiaohan6969 <948309461@qq.com>
2020-09-14 17:53:48 +0800

    1

--- template/login/login.js
+++ template/login/login.js
@@ -1,11 +1,12 @@
 function login(){
     var user_name  = $("#user_name").val()
     var password  = $("#password").val()
+    var str = window.sessionStorage;
     $.ajax({
         headers: {
             "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiLmlrDkurpfMDA4NTE2SkoiLCJleHAiOjE2MDAxMzA3Nzd9.gtOOxV2JlOkRHzbeYBwMWq_tVDWCSuMQetwB13l6fis"
         },
-        url: "http://localhost:8090/mini/user/login/",
+        url: str.getItem("url")+"/mini/user/login/",
         type: 'POST',
         contentType: "application/json",
         data:JSON.stringify({user_name:user_name,pass_word:password}),
