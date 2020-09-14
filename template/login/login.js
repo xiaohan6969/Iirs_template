3f7e63a08a80da86a6c02e11a599f52b0b13fcb9
xiaohan6969 <948309461@qq.com>
2020-09-14 17:15:48 +0800

    1

---
+++ template/login/login.js
@@ -0,0 +1,26 @@
+function login(){
+    var user_name  = $("#user_name").val()
+    var password  = $("#password").val()
+    $.ajax({
+        headers: {
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiLmlrDkurpfMDA4NTE2SkoiLCJleHAiOjE2MDAxMzA3Nzd9.gtOOxV2JlOkRHzbeYBwMWq_tVDWCSuMQetwB13l6fis"
+        },
+        url: "http://localhost:8090/mini/user/login/",
+        type: 'POST',
+        contentType: "application/json",
+        data:JSON.stringify({user_name:user_name,pass_word:password}),
+        success: function (res) {
+            console.log(res)
+            console.log(res.token)
+            var storage = window.sessionStorage;
+            storage.setItem('token', res.token);
+            window.location.href='index.html';
+            // window.location.href="http://www.baidu.com"; //在原有窗口打开
+        },
+        error: function (res) {
+            console.log(res)
+        }
+    })
+
+}
+
