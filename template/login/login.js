b7d7cb219fd2f07d668fe83a17d3eeeab45aa35f
xiaohan6969 <948309461@qq.com>
2020-09-14 17:43:44 +0800

    1

--- template/login/login.js
+++ template/login/login.js
@@ -5,7 +5,7 @@ function login(){
         headers: {
             "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiLmlrDkurpfMDA4NTE2SkoiLCJleHAiOjE2MDAxMzA3Nzd9.gtOOxV2JlOkRHzbeYBwMWq_tVDWCSuMQetwB13l6fis"
         },
-        url: "http://wtthappy.xyz/mini/user/login/",
+        url: "http://localhost:8090/mini/user/login/",
         type: 'POST',
         contentType: "application/json",
         data:JSON.stringify({user_name:user_name,pass_word:password}),
