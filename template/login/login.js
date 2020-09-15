9cbf950a4ff5ba556bd33752b7c3bffb7078404a
xiaohan6969 <948309461@qq.com>
2020-09-15 11:54:29 +0800

    1

--- template/login/login.js
+++ template/login/login.js
@@ -3,10 +3,7 @@ function login(){
     var password  = $("#password").val()
     var str = window.sessionStorage;
     $.ajax({
-        headers: {
-            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiLmlrDkurpfMDA4NTE2SkoiLCJleHAiOjE2MDAxMzA3Nzd9.gtOOxV2JlOkRHzbeYBwMWq_tVDWCSuMQetwB13l6fis"
-        },
-        url: "http://wtthappy.xyz"+"/mini/user/login/",
+        url: str.getItem("domain_name")+"/mini/user/login/",
         type: 'POST',
         contentType: "application/json",
         data:JSON.stringify({user_name:user_name,pass_word:password}),
@@ -15,13 +12,12 @@ function login(){
             console.log(res.token)
             var storage = window.sessionStorage;
             storage.setItem('token', res.token);
-            window.location.href='index.html';
+            window.location.href='../index/index.html';
             // window.location.href="http://www.baidu.com"; //在原有窗口打开
         },
         error: function (res) {
-            console.log(res)
+            alert(res)
         }
     })
-
 }
 
